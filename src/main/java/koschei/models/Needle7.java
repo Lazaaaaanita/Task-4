c17b151c95cb14f01f0b7cfffd71c7a54465f07c
lazanita <157325135+lazanita@users.noreply.github.com>
2024-09-19 03:49:17 +0300

    Task 2.1.4 realization

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.stereotype.Component;
 
+
+public class Needle7 {
+private Deth8 deth;
+    public Needle7(Deth8 deth){this.deth=deth;}
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " ;
     }
 }
